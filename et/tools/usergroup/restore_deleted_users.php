e5c9f0bccf625d30538c78437e54a007efa20ca2
phpBBEesti <argol76@gmail.com>
2019-01-16 09:27:26 +0200

    Update restore_deleted_users.php

--- et/tools/usergroup/restore_deleted_users.php
+++ et/tools/usergroup/restore_deleted_users.php
@@ -6,6 +6,7 @@
 * @copyright (c) 2009 phpBB Group
 * @license http://opensource.org/licenses/gpl-license.php GNU Public License
 *
+* Estonian translation by phpBBestonia.eu <https://www.phpbbestonia.eu>
 */
 
 /**
@@ -38,19 +39,19 @@ if (empty($lang) || !is_array($lang))
 //
 
 $lang = array_merge($lang, array(
-	'DAMAGED_POSTS'			=> 'Damaged Posts',
-	'DAMAGED_POSTS_EXPLAIN'	=> 'The following post IDs contain user information that is too damaged to restore. Please visit the <a href="https://www.phpbb.com/community/viewforum.php?f=46">support forums</a> to receive assistance to resolve this issue.',
+	'DAMAGED_POSTS'			=> 'Kahjustatud postitused',
+	'DAMAGED_POSTS_EXPLAIN'	=> 'Järgmised postituste ID-d sisaldavad kasutajate andmeid, mis on taastamiseks liiga kahjustatud. Probleemi lahendamiseks ja abi saamiseks külastage <a href="https://www.phpbb.com/community/viewforum.php?f=46">tugifoorumeid</a>.',
 
-	'NO_DELETED_USERS'	=> 'There are no deleted users that can be restored',
-	'NO_USER_SELECTED'	=> 'No users selected!',
+	'NO_DELETED_USERS'	=> 'Ühtegi kustutatud kasutajat ei saa taastada',
+	'NO_USER_SELECTED'	=> 'Ühtegi kasutajat ei ole valitud!',
 
-	'RESTORE_DELETED_USERS'						=> 'Restore Deleted Users',
-	'RESTORE_DELETED_USERS_CONFLICT'			=> 'Restore Deleted Users :: Conflicted',
-	'RESTORE_DELETED_USERS_CONFLICT_EXPLAIN'	=> 'This tool allows you to restore users that are deleted from the board but still have "guest" posts on the board.<br />These users will be assigned a random password that you must reset manually after the tool has been run; this tool does <b>not</b> provide a list of these generated passwords.<br /><br />During the last run this tool found some usernames that already exist on this board. Please provide a new name for these users.',
-	'RESTORE_DELETED_USERS_EXPLAIN'				=> 'This tool allows you to restore users that are deleted from the board but still have "guest" posts on the board.<br />These users will be assigned a random password that you must reset manually after the tool has been run; this tool does <b>not</b> provide a list of these generated passwords.',
+	'RESTORE_DELETED_USERS'						=> 'Taasta kustutatud kasutajad',
+	'RESTORE_DELETED_USERS_CONFLICT'			=> 'Taasta kustutatud kasutajad :: Konfliktid',
+	'RESTORE_DELETED_USERS_CONFLICT_EXPLAIN'	=> 'See tööriist võimaldab teil taastada kasutajaid, kes on foorumist kustutatud, kuid nende postitused on ikka veel alles külaliste nime all.<br />Neile kasutajatele antakse parool, mis tuleb pärast tööriista käivitamist käsitsi lähtestada; see tööriist <b>ei</b> nende genereeritud paroolide listi.<br /><br />Viimase töö käigus leiti mõningaid kasutajanimed, mis on selles foorumis juba olemas. Palun andke nendele kasutajatele uus nimi.',
+	'RESTORE_DELETED_USERS_EXPLAIN'				=> 'See tööriist võimaldab teil taastada kasutajaid, kes on foorumist kustutatud, kuid neil on ikka veel külaliste postitused.<br />Neile kasutajatele määratakse juhuslik parool, mis tuleb pärast tööriista käivitamist käsitsi lähtestada; see tööriist <b>ei</b> nende genereeritud paroolide listi.',
 
-	'SELECT_USERS'	=> 'Select users to restore',
+	'SELECT_USERS'	=> 'Valige kasutajad, keda taastada',
 
-	'USER_RESTORED_SUCCESSFULLY'	=> 'The selected user has been restored successfully.',
-	'USERS_RESTORED_SUCCESSFULLY'	=> 'The selected users have been restored successfully.',
+	'USER_RESTORED_SUCCESSFULLY'	=> 'Valitud kasutaja on edukalt taastatud.',
+	'USERS_RESTORED_SUCCESSFULLY'	=> 'Valitud kasutajad on edukalt taastatud.',
 ));
