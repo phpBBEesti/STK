dd25d4758022966568d0385eec5a5080bdbb4111
phpBBEesti <argol76@gmail.com>
2019-01-21 14:08:03 +0200

    Update clear_black_list.php

--- et/tools/admin/clear_black_list.php
+++ et/tools/admin/clear_black_list.php
@@ -6,6 +6,7 @@
 * @copyright (c) 2009 phpBB Group
 * @license http://opensource.org/licenses/gpl-license.php GNU Public License
 *
+* Estonian translation by phpBBestonia.eu <https://www.phpbbestonia.eu>
 */
 
 /**
@@ -38,6 +39,6 @@ if (empty($lang) || !is_array($lang))
 //
 
 $lang = array_merge($lang, array(
-	'CLEAR_BLACK_LIST'				=> 'IP-addresses Black list',
-	'NO_BAN_CELL'					=> 'No banned IP addresses'
+	'CLEAR_BLACK_LIST'				=> 'IP-aadresside Must list',
+	'NO_BAN_CELL'					=> 'Keelatud IP-aadressid'
 ));
